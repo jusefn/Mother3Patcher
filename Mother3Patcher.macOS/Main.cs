ae10a2b532677797bbdc19553f585f498bd4f72f
Jusef N <jusef.n@hotmail.com>
2020-10-24 15:28:05 +0200

    Adding project files to git

---
+++ Mother3Patcher.macOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Mother3Patcher.macOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ Mother3Patcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mother3Patcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
